f75f46bbb96c1b235f74b67438cd41b7c8580c41
kanavsachdeva41 <kanavsachdeva41@gmail.com>
2026-06-14 12:06:05 +0530

    Question

---
+++ DAY74.c
@@ -0,0 +1,73 @@
+/*Problem Statement
+Given an array of candidate names where each name represents a vote cast for that candidate, determine the candidate who received the maximum number of votes. In case of a tie, return the lexicographically smallest candidate name.
+
+Input Format
+First line contains an integer n representing number of votes.
+Second line contains n space-separated strings representing candidate names.
+
+Output Format
+Print the name of the winning candidate followed by the number of votes received.
+
+Sample Input
+13
+john johnny jackie johnny john jackie jamie jamie john johnny jamie johnny john
+
+Sample Output
+john 4
+
+Explanation
+Both john and johnny receive 4 votes, but john is lexicographically smaller, so john is declared the winner.*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char names[n][101];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", names[i]);
+    }
+
+    char candidate[n][101];
+    int votes[n];
+    int uniqueCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        int found = -1;
+
+        for (int j = 0; j < uniqueCount; j++) {
+            if (strcmp(names[i], candidate[j]) == 0) {
+                found = j;
+                break;
+            }
+        }
+
+        if (found != -1) {
+            votes[found]++;
+        } else {
+            strcpy(candidate[uniqueCount], names[i]);
+            votes[uniqueCount] = 1;
+            uniqueCount++;
+        }
+    }
+
+    char winner[101];
+    int maxVotes = 0;
+
+    for (int i = 0; i < uniqueCount; i++) {
+        if (votes[i] > maxVotes) {
+            maxVotes = votes[i];
+            strcpy(winner, candidate[i]);
+        } else if (votes[i] == maxVotes) {
+            if (strcmp(candidate[i], winner) < 0) {
+                strcpy(winner, candidate[i]);
+            }
+        }
+    }
+
+    printf("%s %d\n", winner, maxVotes);
+
+    return 0;
+}
